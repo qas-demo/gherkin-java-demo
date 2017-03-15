3d5974668622d7980aef86ab8ceef70866067d27
Bobby Smith <bobby.smith@gmail.com>
2017-03-15 19:39:14 -0400

    add cucumber runner

---
+++ src/test/java/com/pulsedemo/steps/CukeRunner.java
@@ -0,0 +1,12 @@
+package com.pulsedemo.steps;
+ 
+import org.junit.runner.RunWith;
+ 
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+ 
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "classpath:features", plugin = { "pretty", "html:target/cucumber-html-report" }, tags = {})
+public class CukeRunner {
+
+}
